b03fc5d279954e209dec0b2e35c784ef8ad6839c
Raviraj <ravirajghodake98@gmail.com>
2023-12-18 18:22:42 +0530

    global error handler

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const campRouter = require('./routes/campRoute');
 
 const app = express();
@@ -7,5 +10,11 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/camps', campRouter)
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404))
+})
+
+app.use(globalErrorHandler);
 
 module.exports = app;
